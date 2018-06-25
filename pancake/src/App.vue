7dbc79ad73f7978b9f3c92daeee6c001cfbb8340
awestlib <awest@fontanalib.org>
2018-06-25 16:47:50 -0400

    Added Product.vue with socks & bacon

--- pancake/src/App.vue
+++ pancake/src/App.vue
@@ -2,16 +2,17 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Product title="Socks" v-bind:product="{price: '7.33', description: 'Socks for sale!', img:'http://baconmockup.com/300/300'}"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Product from './components/Product.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, Product
   }
 }
 </script>
---
+++ pancake/src/components/Product.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="product" class="product">
+    <img :src = "product.img" class="product_img"/>
+    <h2 class="product_title">{{ title }}</h2>
+    <h3 class="product_price">${{ product.price }}</h3>
+    <p class="product_description"> {{ product.description }}</p>
+</div>
+</template>
+<script>
+export default {
+    name: 'product',
+    props: {
+        title: String,
+        product: Object 
+    }
+}
+</script>
+<style scoped>
+.product {
+    text-align:left;
+    border:1px solid red;
+    padding:1em;
+    margin:2em 1em;
+}
+.product:after {
+    content:"";
+    display:block;
+    clear:both;
+}
+.product_title {
+    color: #666;
+    font-size:200%;
+    margin-bottom:0;
+}
+.product_price {
+    margin-top:0;
+}
+.product_img {
+    float:right;
+    max-width:100%;
+    height:auto;
+}
+</style>
